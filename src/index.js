0a1885eee8b64c69b3980fddac8f0237345c953e
Jerome-gir <jerome.gir@gmail.com>
2022-04-04 10:23:09 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import App from './components/App/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Provider store={store}>
+         <App /> 
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
