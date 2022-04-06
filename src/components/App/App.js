a79e84f7f0eb62d9b3c4ab24a0e15bca22c17bd9
Jerome-gir <jerome.gir@gmail.com>
2022-04-06 23:05:39 +0200

    stay connected with jwt token

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { checkUser } from '../../actions/user';
 // import Home from '../Home/Home';
 // import HomeApp from '../HomeApp/HomeApp';
 // import NavBar from '../NavBar/NavBar';
@@ -7,6 +9,16 @@ import Login from '../Login';
 import './style.scss';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // recup token in localStorage
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(checkUser());
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       {/* <Home /> */}
