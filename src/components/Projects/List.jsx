a7fddbac9c116b76ecc2385e795343f4a7fc2aed
Jerome-gir <jerome.gir@gmail.com>
2022-04-07 23:48:32 +0200

    structuration

---
+++ src/components/Projects/List.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+import { React } from 'react';
+import data from './ListData.json';
+
+function List(props) {
+  // create a new array by filtering the original array
+  const filteredData = data.filter((el) => {
+    if (props.input === '') {
+      return el;
+    }
+    // return the item which contains the user input
+
+    return el.text.toLowerCase().includes(props.input);
+  });
+  return (
+    <ul className="projects-list">
+      {filteredData.map((item) => (
+        <li key={item.id}>
+          {item.text}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,41 +1,32 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import { React, useState } from 'react';
+import TextField from '@mui/material/TextField';
 import ProjectsHeader from './ProjectsHeader';
+import List from './List';
 import './style.scss';
 
 function Projects() {
+  const [inputText, setInputText] = useState('');
+  const inputHandler = (e) => {
+    // convert input text to lower case
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
     <div className="projects">
       <ProjectsHeader />
-      <main className="projects-main">
-        <div className="projects-container_list">
-          <ul>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-            <li className="projects-main_li">
-              <p>Nom du projet</p>
-            </li>
-          </ul>
-        </div>
-      </main>
+      <div className="projects__container__list">
+        <TextField
+          className="projects-searchBar"
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+        <List input={inputText} />
+      </div>
     </div>
   );
 }
