b52f3a79fb104aac6b1b6713d98865b477e013a0
maxime-trevisiol <92717713+maxime-trevisiol@users.noreply.github.com>
2022-04-10 18:02:56 +0200

    add status icon ListProjets

--- src/components/Client/ListProjets.jsx
+++ src/components/Client/ListProjets.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import { React, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { createTheme } from '@mui/material/styles';
 import data from './data.json';
+import './style.scss';
 
-function ListProjets(props) {
+function ListProjets() {
+  // créer un thème personnalisé pour les status
+  // définir nom des status
+
   return (
     <ul className="client-listProjets">
       {data.map((item) => (
         <Link to={`/projets/${item.id}`}>
-          <li key={item.id}>{item.title} {item.description} {item.status} </li>
+          <li key={item.id}>{item.title}-{item.description}-{item.status} <Chip size="small" label={item.status} color="primary" /></li>
         </Link>
       ))}
     </ul>
