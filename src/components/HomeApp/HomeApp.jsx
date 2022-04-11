6be40559d06b14a037449c68a91d8b4c7f012c72
maxime-trevisiol <92717713+maxime-trevisiol@users.noreply.github.com>
2022-04-11 14:38:51 +0200

    example filter date

--- src/components/HomeApp/HomeApp.jsx
+++ src/components/HomeApp/HomeApp.jsx
@@ -2,11 +2,49 @@ import React, { useState, useEffect } from 'react';
 import HomeAppHeader from './HomeAppHeader';
 import './style.scss';
 
+// const interventions = [
+//   {
+//     date: new Date(2022, 3, 11),
+//   },
+//   {
+//     date: new Date(2022, 3, 12),
+//   },
+//   {
+//     date: new Date(2022, 3, 10),
+//   },
+//   {
+//     date: new Date(2022, 3, 11),
+//   },
+//   {
+//     date: new Date(2022, 3, 11),
+//   },
+//   {
+//     date: new Date(2022, 3, 11),
+//   },
+// ];
+
+// const formatDate = (date) => {
+//   const day = date.getDate();
+//   const month = date.getMonth();
+//   const year = date.getFullYear();
+
+//   return `${day}/${month}/${year}`;
+// };
+
+// => https://stackoverflow.com/questions/11955281/sql-where-datetime-column-equals-todays-date
+
 function HomeApp() {
   const [dateState, setDateState] = useState(new Date());
   useEffect(() => {
     setInterval(() => setDateState(new Date()), 30000);
   }, []);
+
+  // const filteredInterventions = interventions.filter((intervention) => {
+  //   return formatDate(intervention.date) === formatDate(new Date());
+  // });
+
+  // console.log(filteredInterventions);
+
   return (
     <div className="homeApp">
       <HomeAppHeader />
