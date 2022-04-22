648ef1876bfc7bda5b0383b37a625ce79f1125a6
Your NameStevenYapi <steven.persos@gmail.com>
2022-04-22 18:57:18 +0200

    error update

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './style.scss';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Serveur from '../../assets/serveur.jpg';
 
 function Error() {
   return (
-    <p>Page 404</p>
+    <Card>
+      <CardMedia
+        component="img"
+        height="500"
+        src={Serveur}
+        alt="page 404"
+      />
+      <CardContent>
+        <Typography>
+          Il semblerait que la page demandée ne soit pas présente ou accessible quel dommage...
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
