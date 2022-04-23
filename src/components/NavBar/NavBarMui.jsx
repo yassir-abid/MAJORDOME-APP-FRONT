aba37bd7f5970ab44016da79f96aebf71b4ad3de
maxime-trevisiol <92717713+maxime-trevisiol@users.noreply.github.com>
2022-04-23 17:53:56 +0200

    add subtitle icon nav

--- src/components/NavBar/NavBarMui.jsx
+++ src/components/NavBar/NavBarMui.jsx
@@ -6,6 +6,7 @@ import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
@@ -33,6 +34,7 @@ export default function BottomAppBar() {
           borderTopRightRadius: '17px',
           top: 'auto',
           bottom: 0,
+          height: 65,
         }}
       >
         <Container maxWidth="md">
@@ -40,37 +42,67 @@ export default function BottomAppBar() {
             sx={{
               display: 'flex',
               flexDirection: 'row',
-              justifyContent: 'space-between',
+              justifyContent: 'space-around',
             }}
           >
             <NavLink to="/home-app">
-              <IconButton color="inherit" aria-label="open drawer">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <HomeIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Accueil</Typography>
               </IconButton>
             </NavLink>
             <NavLink to="/clients">
-              <IconButton color="inherit" aria-label="open drawer">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <GroupAddIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Clients</Typography>
               </IconButton>
             </NavLink>
             <NavLink to="/schedule">
-              <IconButton color="inherit">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <CalendarTodayIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Agenda</Typography>
               </IconButton>
             </NavLink>
             <NavLink to="/documents">
-              <IconButton color="inherit">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <NoteAddIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Docs</Typography>
               </IconButton>
             </NavLink>
             <NavLink to="/projects">
-              <IconButton color="inherit">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <EngineeringIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Projets</Typography>
               </IconButton>
             </NavLink>
             <NavLink to="/interventions">
-              <IconButton color="inherit">
+              <IconButton
+                sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                color="inherit"
+                aria-label="open drawer"
+              >
                 <HandymanIcon fontSize="large" />
+                <Typography sx={{ fontSize: 8 }}>Intervention</Typography>
               </IconButton>
             </NavLink>
           </Toolbar>
