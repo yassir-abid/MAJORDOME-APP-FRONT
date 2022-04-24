e389606dfac5a041cc466aa0c73f239625e5c81b
Your NameStevenYapi <steven.persos@gmail.com>
2022-04-24 18:11:15 +0200

    update page 404

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -6,28 +6,30 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Serveur from '../../assets/serveur3.png';
+import Serveur from '../../assets/serveur4.jpg';
 
 function Error() {
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="500"
-        src={Serveur}
-        alt="page 404"
-      />
-      <CardContent>
-        <Typography>
-          Il semblerait que la page demandée ne soit pas présente ou accessible quel dommage...
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Home
-        </Button>
-      </CardActions>
-    </Card>
+    <div className="error">
+      <Card>
+        <CardMedia
+          component="img"
+          height="550"
+          src={Serveur}
+          alt="page 404"
+        />
+        <CardContent>
+          <Typography>
+            Il semblerait que la page demandée ne soit pas présente ou accessible quel dommage...
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="outlined" size="small" color="primary" href="/login">
+            Home
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
