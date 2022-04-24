ceaded4e99685972f50a22fabe09e78cb122780f
maxime-trevisiol <92717713+maxime-trevisiol@users.noreply.github.com>
2022-04-24 20:31:20 +0200

    create page equipement & besoin component Mui

--- src/components/Client/Equipments/Equipments.jsx
+++ src/components/Client/Equipments/Equipments.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Typography } from '@mui/material';
 
 // import ClientHeader from '../ClientHeader';
-import '../style.scss';
+// import '../style.scss';
 
 function Equipments() {
   // équipements
@@ -20,32 +21,49 @@ function Equipments() {
 
   return (
     <div>
+
+      <Box sx={{
+        bgcolor: 'primary.main',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderBottomLeftRadius: '17px',
+        borderBottomRightRadius: '17px',
+        height: 60,
+        p: 1,
+      // FIXME: gérer le titre en center et l'avatar en start (left)
+      }}
+      >
+        <Box>
+          <Typography variant="h6" gutterBottom component="div" sx={{ color: 'white' }}>
+            Équipements & Besoins
+          </Typography>
+        </Box>
+      </Box>
+
       {/* <ClientHeader /> */}
       <div>
         <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
+          sx={{ m: 1 }}
         >
           <div>
             <TextField
+              sx={{ mt: 4, mb: 1 }}
               id="Equipments"
               label="Équipements"
               multiline
-              minRows={4}
+              minRows={5}
               maxRows={10}
               value={eqValue}
               onChange={handleEqChange}
               fullWidth
             />
             <TextField
+              sx={{ mb: 1 }}
               id="Besoins"
               label="Besoins"
               multiline
-              minRows={4}
+              minRows={5}
               maxRows={10}
               value={bsnValue}
               onChange={handleBsnChange}
